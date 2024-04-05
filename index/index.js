df641b15a1dfeceb128628431e33b8dd1e7f47c4
linamatiz <linamatiz2022@outlook.com>
2024-04-04 21:56:49 -0500

    icons

--- index/index.js
+++ index/index.js
@@ -1,9 +1,34 @@
-// Inicializa el mapa
-var mymap = L.map('mapid').setView([4.5709, -74.2973], 6); // Colombia: Coordenadas del centro de Colombia y un nivel de zoom apropiado
-// Agrega un mosaico de mapa base (tile layer) a nuestro mapa
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
-// Agrega un marcador al mapa en una ubicación específica (por ejemplo, Bogotá)
-var marker = L.marker([4.6097, -74.0817]).addTo(mymap); // Bogotá, Colombia
+var map = L.map('mapid').setView([44.0462, -123.022], 13);
 
+        // mapa
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // icono 
+        var customIcon = L.icon({
+            iconUrl: 'http://127.0.0.1:5500/assets/simpson.ico',
+            iconSize: [38, 38], // pixeles 
+            iconAnchor: [19, 38], // anclaje icono
+            popupAnchor: [0, -38] // Punto de anclaje del popu
+        });
+        
+        
+
+        // marcador personalizado
+        L.marker([44.0462, -123.022], { icon: customIcon }).addTo(map)
+            .bindPopup('¡HI HOMER!') ; // se agrega un popup al marcador
+
+
+            var customIcon = L.icon({
+            iconUrl: 'http://127.0.0.1:5500/assets/Family-Guy-PNG-Photos.ico',
+            iconSize: [38, 38], // pixeles 
+            iconAnchor: [19, 38], 
+            popupAnchor: [0, -38] // Punto de anclaje del popu
+        });
+        
+        
+
+        // marcador personalizado
+        L.marker([44.0462, -122.920], { icon: customIcon }).addTo(map)
+            .bindPopup('¡HI PETTER!') ; // MENSAJE PERSONALIZADO
